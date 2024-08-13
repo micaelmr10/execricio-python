370f7a1010eb361bd6566f51b4c26416dd2d93e9
micaelmr10 <00001155042244sp@al.educacao.sp.gov.br>
2024-08-13 20:24:36 -0300

    Create exercicio04.py
    adicionado o exercicio04

---
+++ exercicio04.py
@@ -0,0 +1,10 @@
+n1 = float(input("digite o primeiro numero:"))
+n2 = float(input("digite o segundo numero:"))
+
+if (n1 > n2):
+  maior = n1
+else:
+  maior = n2
+
+print(f"o maior numero é {maior}")
+  
