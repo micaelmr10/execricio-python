aa9805dccc81493b02bb3d4d60f5ef542ae725ab
micaelmr10 <00001155042244sp@al.educacao.sp.gov.br>
2024-08-28 18:02:52 -0300

    Create exercicio28.py
    exercicio28

---
+++ exercicio28.py
@@ -0,0 +1,6 @@
+n1 = float(input("digite o primeiro numero: "))
+n2 = float(input("digite o segundo numero: "))
+n3 = float(input("digite o terceiro numero: "))
+
+if (n1 > n2 and n1 > n3):
+  print("o maior numero é", n1,n2,n3)
