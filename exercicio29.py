ac56325d3473d133d9bfacf95e3afc00c88e52bb
micaelmr10 <00001155042244sp@al.educacao.sp.gov.br>
2024-08-28 18:04:10 -0300

    Create exercicio29.py
    exercicio29

---
+++ exercicio29.py
@@ -0,0 +1,8 @@
+
+n1 = float(input("digite o primeiro numero: "))
+n2 = int(input("digite o segundo numero: "))
+n3 = int(input("digite o terceiro numero: "))
+
+multi = n1 * n2 * n3
+
+print("a multiplicacao é:",multi)
