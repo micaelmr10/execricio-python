5c3428341ffb9c0b1ebc8cc90a7d4d03ae8acb0d
micaelmr10 <00001155042244sp@al.educacao.sp.gov.br>
2024-08-28 20:08:34 -0300

    Create exercicio31.py
    3'

---
+++ exercicio31.py
@@ -0,0 +1,9 @@
+num1 = int(input("Digite o primeiro número inteiro: "))
+num2 = int(input("Digite o segundo número inteiro: "))
+num3 = float(input("Digite o número real: "))
+soma = num1 + num2
+produto = num1 * num2
+divisao = num3 / num2
+print(f"A soma dos dois números inteiros é: {soma}")
+print(f"O produto dos dois números inteiros é: {produto}")
+print(f"A divisão do número real pelo segundo inteiro é: {divisao}")
