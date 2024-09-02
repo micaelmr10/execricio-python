957f459dae2fafb792cfba7bb088858d6d12356d
micaelmr10 <00001155042244sp@al.educacao.sp.gov.br>
2024-09-02 18:29:11 -0300

    Create exercicio33.py

---
+++ exercicio33.py
@@ -0,0 +1,11 @@
+num1 = int(input("digite o primeiro numero inteiro: "))
+num2 = int(input("digite o segundo numero inteiro: "))
+num3 = int(input("digite o número real: "))
+
+produto = (num1 * 2) * (num2 / 2)
+soma = (num1 * 3) + num2
+cubo = num3 ** 3
+
+print(f"o produto do dobro do primeiro com metade do segundo é: {produto}")
+print(f"a soma do triplo do primeiro com o terceiro é: {soma}")
+print(f"o terceiro elevado ao cubo é: {cubo}")
