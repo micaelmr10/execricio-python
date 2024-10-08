b434229c577b8113c9daf3f410dc5c0ff1cc8798
micaelmr10 <00001155042244sp@al.educacao.sp.gov.br>
2024-10-08 18:59:11 -0300

    Create exercicio70.py

---
+++ exercicio70.py
@@ -0,0 +1,12 @@
+def fizzbuzz(n):
+    if n % 3 == 0 and n % 5 == 0:
+        return "FizzBuzz"
+    elif n % 3 == 0:
+        return "Fizz"
+    elif n % 5 == 0:
+        return "Buzz"
+    else:
+        return str(n)
+
+n = int(input())
+print(fizzbuzz(n))
